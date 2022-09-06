f7d46eb0637c83ba01833c838aba0e8b7055e3b8
Will Wang <willy.j.wang@gmail,com>
2022-09-06 11:16:21 -0500

    Complete iteration 3

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -3,9 +3,28 @@ class Dealership
   def initialize(name, address)
     @inventory = []
     @inventory_count = 0
+    @address = address
   end
   def add_car(car)
     @inventory << car
     @inventory_count += 1
+  end
+  def has_inventory?
+    !@inventory.empty?
+  end
+  def cars_by_make(make)
+    @inventory.select do |car|
+      car.make == make
+    end
+  end
+  def total_value
+    value = 0
+    @inventory.each do |car|
+      value += car.total_cost
+    end
+    value
+  end
+  def details
+    {"total_value" => total_value, "address" => @address}
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -25,4 +25,65 @@ RSpec.describe Dealership do
       expect(dealership.inventory_count).to eq(2)
     end
   end
+  describe '#has_inventory' do
+    it 'can tell if it does not have inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      expect(dealership.has_inventory?).to eq(false)
+    end
+    it 'can tell if it has inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.has_inventory?).to eq(true)
+    end
+  end
+  describe '#cars_by_make' do
+    it 'can separate inventory cars by make' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.cars_by_make("Toyota")).to eq([car_2, car_3])
+      expect(dealership.cars_by_make("Ford")).to eq([car_1])
+    end
+  end
+  describe '#total_value' do
+    it 'can add up total value of all cars in inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.total_value).to eq(156000)
+    end
+  end
+  describe '#details' do
+    it 'can return a hash of dealership details' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+      expect(dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
+    end
+  end
 end
